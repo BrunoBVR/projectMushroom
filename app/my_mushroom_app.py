d636dd174a70dfc6f621a022ec0e70c20fbf438c
BrunoBVR <bruno64bits@gmail.com>
2021-03-25 00:55:45 -0300

    Deleted unused imports in app

--- app/my_mushroom_app.py
+++ app/my_mushroom_app.py
@@ -6,7 +6,7 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import confusion_matrix,classification_report,plot_confusion_matrix
+from sklearn.metrics import confusion_matrix,classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.tree import plot_tree
